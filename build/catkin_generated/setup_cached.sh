a864e294ec627aba9373562bfa3c316699e6e595
takeshi-pumas <robottakeshi@gmail.com>
2024-04-15 20:38:11 -0600

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/oscar/turtlebot/catkin_ws/devel/lib:/home/oscar/catkin_extras/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda-11.5/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.5/bin:/home/oscar/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/oscar/turtlebot/catkin_ws/build'
